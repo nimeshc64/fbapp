73b7c6e9eb5f0a7c99329973aeeb4fe11a8e90db
nimeshc64 <nimeshc64@gmail.com>
2016-01-26 09:59:46 +0530

    aaa

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+@tutorial link : http://www.programmer.lk/facebook-application-development-i/
+
+PROGRAMMER.LK
--- index.php
+++ index.php
@@ -1,81 +1,107 @@
 <?php
-session_start();
-require_once __DIR__ . '/src/Facebook/autoload.php';
+######### edit details ##########
+$appId              = '537180863122064'; //Facebook App ID
+$appSecret          = 'f1f866dafe0fca19eba0e88945633a95'; // Facebook App Secret
+$return_url         = 'https://dewz.esy.es/';  //return url (url to script)
+$temp_folder        = 'tmp/'; //temp dir path to store images
+$image_id_png       = 'assets/commen.png'; //  image template path
+$font               = 'assets/fonts/DidactGothic.ttf'; //font used
+#################################
 
-$fb = new Facebook\Facebook([
-  'app_id' => '537180863122064',
-  'app_secret' => 'f1f866dafe0fca19eba0e88945633a95',
-  'default_graph_version' => 'v2.4',
-  ]);
 
-$helper = $fb->getRedirectLoginHelper();
+// check if curl is enabled
+if (!in_array  ('curl', get_loaded_extensions())){die('curl required!');} 
 
-$permissions = ['email']; // optional
-	
-try {
-	if (isset($_SESSION['facebook_access_token'])) {
-		$accessToken = $_SESSION['facebook_access_token'];
-	} else {
-  		$accessToken = $helper->getAccessToken();
-	}
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
- 	// When Graph returns an error
- 	echo 'Graph returned an error: ' . $e->getMessage();
+//include facebook SDK
+include_once("inc/facebook.php"); 
 
-  	exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
- 	// When validation fails or other local issues
-	echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  	exit;
- }
+//Facebook API
+$facebook = new Facebook(array(
+  'appId'  => $appId,
+  'secret' => $appSecret,
+));
 
-if (isset($accessToken)) {
-	if (isset($_SESSION['facebook_access_token'])) {
-		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-	} else {
-		// getting short-lived access token
-		$_SESSION['facebook_access_token'] = (string) $accessToken;
 
-	  	// OAuth 2.0 client handler
-		$oAuth2Client = $fb->getOAuth2Client();
-
-		// Exchanges a short-lived access token for a long-lived one
-		$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-
-		$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-
-		// setting default access token to be used in script
-		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-	}
-
-	// redirect the user back to the same page if it has "code" GET variable
-	if (isset($_GET['code'])) {
-		header('Location: ./');
-	}
-
-	// getting basic info about user
-	try {
-		$profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
-		$profile = $profile_request->getGraphNode()->asArray();
-	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-		// When Graph returns an error
-		echo 'Graph returned an error: ' . $e->getMessage();
-		session_destroy();
-		// redirecting user back to app login page
-		header("Location: ./");
-		exit;
-	} catch(Facebook\Exceptions\FacebookSDKException $e) {
-		// When validation fails or other local issues
-		echo 'Facebook SDK returned an error: ' . $e->getMessage();
-		exit;
-	}
-	
-	// printing $profile array on the screen which holds the basic info about user
-	print_r($profile);
-
-  	// Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
-} else {
-	// replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
-	$loginUrl = $helper->getLoginUrl('https://dewz.esy.es', $permissions);
-	echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+if(isset($_GET["logout"]) && $_GET["logout"]==1)
+{
+    //Destroy the current session and logout user
+    $facebook->destroySession();
+    header('Location: '.$return_url);
 }
+
+//get facebook user
+$fbuser = $facebook->getUser();
+
+
+//check user session
+if(!$fbuser) 
+{
+    //new users get to see this login button
+    $loginUrl = $facebook->getLoginUrl(array('scope' => $fbPermissions,'redirect_uri'=>$return_url));
+    echo '<div style="margin:20px;text-align:center;"><a href="'.$loginUrl.'"><img src="assets/facebook-login.png" /></a></div>';
+}
+else
+{    
+     //get user profile
+     try {
+        $user_profile = $facebook->api('/me');
+        
+        //list of user granted permissions
+        $user_permissions = $facebook->api("/me/permissions"); 
+      } catch (FacebookApiException $e) {
+        echo $e;
+        $fbuser = null;
+      }
+     
+   
+    //display logout url
+    echo '<div>'.$user_profile["name"].' [<a href="?logout=1">Log Out</a>]</div>'; 
+    ###### start generating ID ##########
+    
+    //copy user profile image from facebook in temp folder
+    if(!copy('http://graph.facebook.com/'.$fbuser.'/picture?width=100&height=100',$temp_folder.$fbuser.'.jpg'))
+    {
+        die('Could not copy image!');
+    }
+
+    ##### start generating Facebook APP Image ########
+    $dest = imagecreatefrompng($image_id_png); // source  image template
+    $src = imagecreatefromjpeg($temp_folder.$fbuser.'.jpg'); //facebook user image stored in our temp folder
+    
+    imagealphablending($dest, false); 
+    imagesavealpha($dest, true);
+    
+    //merge user picture with  image template
+    //need to play with numbers here to get alignment right
+    imagecopymerge($dest, $src, 320, 32, 0, 0, 100, 100, 100); 
+    
+    //colors we use for font
+    $facebook_blue = imagecolorallocate($dest, 255, 255, 255); // Create white color
+    $facebook_grey = imagecolorallocate($dest, 74, 74, 74); // Create grey color
+    
+    //Texts to embed into  image template
+    $txt_user_id        = $fbuser;
+    $txt_user_name      = isset($user_profile['name'])?$user_profile['name']:'No Name';
+//    $txt_user_gender    = isset($user_profile['gender'])?$user_profile['gender']:'No gender';
+    $txt_credit         = 'Developed by www.programmer.lk';
+    
+    $txtLuckyNo = substr($txt_user_id, -1);
+    
+
+    imagealphablending($dest, true); //bring back alpha blending for transperent font
+    
+    imagettftext($dest, 72, 0, 170, 190, $facebook_grey , $font, $txtLuckyNo); //Write Lucky NO 
+    imagettftext($dest, 15, 0, 290, 165, $facebook_grey, $font, $txt_user_name); //Write name 
+ //   imagettftext($dest, 15, 0, 25, 147, $facebook_grey, $font, $txt_user_gender); //Write gender 
+    imagettftext($dest, 8, 0, 25, 270, $facebook_blue, $font, $txt_credit); //Write credit link 
+        
+    imagepng($dest, $temp_folder.'id_'.$fbuser.'.jpg'); //save  in temp folder
+
+	//now we have generated , we can display or post it on facebook
+    echo '<img src="tmp/id_'.$fbuser.'.jpg" >'; //display saved user 
+    
+	
+    imagedestroy($dest);
+    imagedestroy($src);
+}
+?>
